eef1cfda337af7c104464e59797a5ab62e03089d
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-07-09 23:23:33 -0300

    add not found response

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
         })
         return res.writeHead(201).end()
     }
-    return res.end('Hello world!')
+    return res.writeHead(404).end('not found')
 
 })
 
