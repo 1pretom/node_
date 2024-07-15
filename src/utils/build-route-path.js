9e1abdd00c95501c5149c5dc14cd42c6e27e2809
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-07-15 20:10:29 -0300

    add regex to get regex and transform to legible params

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,7 @@
 export const buildRoutePath = (path) => {
     const routeParametersRegex = /:([a-zA-Z]+)/g
-    const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-+]+)')
-    console.log(Array.from(path.matchAll(routeParametersRegex)))
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+    const pathRegex = new RegExp(`^${pathWithParams}`)
+    return pathRegex
 }
