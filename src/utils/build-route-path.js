0c6e1ca2547f756142a673f8945067ac3864064a
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-07-14 21:31:32 -0300

    add regex

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,4 +1,5 @@
 export const buildRoutePath = (path) => {
     const routeParametersRegex = /:([a-zA-Z]+)/g
+    const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-+]+)')
     console.log(Array.from(path.matchAll(routeParametersRegex)))
 }
